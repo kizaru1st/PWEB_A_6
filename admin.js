fc4cdfd8cbb8101960722961ea58dd95f7a879c2
Reza Rizki <rezarizki77@gmail.com>
2022-03-18 08:31:54 +0700

    Rename routerr

--- admin.js
+++ admin.js
@@ -1,11 +1,11 @@
 const express = require('./node_modules/express');
-const routerr = express.Router();
+const routing = express.Router();
 
 // <--- 4 Methods of HTTP -->
-routerr.get('/admin/:id', (req, res) => {
+routing.get('/admin/:id', (req, res) => {
     const ID = req.params.id;
     res.send(`Get Admin id: ${ID}`);
 });
 
 
-module.exports = routerr;
+module.exports = routing;
