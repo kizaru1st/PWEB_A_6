90ab408b68ae9d79a8144767a1960f61a2949d7b
AiniIzzathyIspendi2011522025 <90383696+AiniIzzathyIspendi2011522025@users.noreply.github.com>
2022-04-04 20:26:27 +0700

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var express = require('../..');
 var hash = require('pbkdf2-password')()
 var path = require('path');
 var session = require ('express-session');
+const res = require('express/lib/response');
+const { networkInterfaces } = require('os');
 
 var app = module.export = express();
 
@@ -57,3 +59,90 @@ hash({ password:'foobar' }, function (err, pass, salt, hash){
 
 // Authenticate using our plain-object database of doom!
 
+function authenticate (name, pass, fn){
+
+    if (!module.parent) console.log ('authenticating %s : %s', name, pass);
+    var user = user[name];
+
+    //query the db for the given usernama
+
+    if (!user) return fn(null, null)
+
+    //apply the same algorithm to the POSTed password, applying
+    //the hash against the pass/salt, if there is a match we
+    //found the user
+
+    hash({password: pass, salt: user, salt}, function (err, pass, salt, hash)
+    {
+        if (err) return fn(err);
+        if (hash === user.hash) return fn(null, user)
+        fn(null, null)
+    });
+
+}
+
+
+function restrict (req, rse, next) {
+    if (req.session.user){
+        next();
+    } else {
+        req.sesiion.error = 'Acces Denied!';
+        res.redirect('/login');
+    }
+}
+
+app.get('/', function(req, res){
+    res.redirect('/login');
+});
+
+app.get('/restricted', restrict, function(req, res){
+    res.send('Wahoo! restricted are, click to <a href="/logout">logout</a>');
+});
+
+app.get('/logout', function (req, res){
+    //destroy the user's session to log them out
+    //will be re-created next request
+
+    req.session.destroy(function(){
+        res.redirect('/');
+    });
+});
+
+app.post('/login', function(req, res){
+    res.render('login');
+});
+
+app.post('/login', function(req, res, next){
+    authenticate(req.body.username, req.body.password, function(err, user){
+        if(err) return networkInterfaces(err)
+        if (user){
+            //regenerate session when signing in
+            //to prevent fixation
+
+            req.session.regenerate(function(){
+                //store the user's primary key
+                //in the session store to the retrieved,
+                //or in this case the entire user object
+
+                req.session.user = user;
+                req.session.success = 'Authenticated as' + user.name
+                + 'click to <a href="/logout">logout</a>.'
+                + 'You may now access <a href="/restricted">/restricted</a.>'; 
+                res.redirect('back');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your'
+            +'username and password'
+            +'(use "tj" and "foobar")';
+            res.redirect('/login');
+        }
+    });
+});
+
+/*istanbul ignore next*/
+
+if (!module.parent)
+{
+    app.listen(3000);
+    console.log ('Express started on port 3000');
+}
