25263b04ce0b7fff2ddffa559b29a1f5fdc0b49d
Reza Rizki <rezarizki77@gmail.com>
2022-06-07 19:35:19 +0700

    EJS

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dosen-profile', (req, res) => {
+  res.render('dosen-profile', {
+    layout: 'layouts/dosen-app',
+    title: 'Dashboard - Dosen',
+    judulPage: 'RPS'
+  })
+})
+
+router.get('/dosen-profile-edit', (req, res) => {
+  res.render('dosen-profile-edit', {
+    layout: 'layouts/dosen-app',
+    title: 'Dashboard - Dosen',
+    judulPage: 'RPS'
+  })
+})
+
+router.get('/dosen-rps', (req, res) => {
+  res.render('dosen-rps', {
+    layout: 'layouts/dosen-app',
+    title: 'Dashboard - Dosen',
+    judulPage: 'RPS'
+  })
+})
+
 module.exports = router;
