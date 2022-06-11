b02f48caa66ab321ced5e6936065864afe98a5d6
AiniIzzathyIspendi2011522025 <90383696+AiniIzzathyIspendi2011522025@users.noreply.github.com>
2022-06-11 23:45:36 +0700

    gogogo

--- controllers/dosenController.js
+++ controllers/dosenController.js
@@ -1,5 +1,44 @@
 module.exports = {
+    viewRPS: (req, res) => {
+      res.render("dosen/view_rps");
+    },
     viewDosenRPS: (req, res) => {
-        res.render('dosen/dosen-rps');
-    }
-} 
+      res.render("dosen/view_dosen-rps");
+    },
+    viewDosenCPMK: (req, res) => {
+      res.render("dosen/view_dosen-cpmk");
+    },
+    viewDosenInfoRPS: (req, res) => {
+      res.render("dosen/view_dosen-info-rps");
+    },
+    viewDosenKomponenNilaiTampil: (req, res) => {
+      res.render("dosen/view_dosen-komponen-nilai-tampil");
+    },
+    viewDosenPertemuan: (req, res) => {
+      res.render("dosen/view_dosen-pertemuan");
+    },
+    viewDosenReferensiA: (req, res) => {
+      res.render("dosen/view_dosen-referensi-a");
+    },
+    viewDosenTambahCPMK: (req, res) => {
+      res.render("dosen/view_dosen-tambah-cpmk");
+    },
+    viewDosenTambahKomponen: (req, res) => {
+      res.render("dosen/view_dosen-tambah-komponen");
+    },
+    viewDosenTambahPertemuan: (req, res) => {
+      res.render("dosen/view_dosen-tambah-pertemuan");
+    },
+    viewDosenTambahReferensi: (req, res) => {
+      res.render("dosen/view_dosen-tambah-referensi");
+    },
+    viewDosenTambahRPS: (req, res) => {
+      res.render("dosen/view_dosen-tambah-rps");
+    },
+  
+  
+  
+  
+  
+  };
+  
--- routes/dosen.js
+++ routes/dosen.js
@@ -1,6 +1,17 @@
-const router = require('express').Router();
-const dosenController = require('../controllers/dosenController'); 
+const router = require("express").Router();
+const dosenController = require("../controllers/dosenController");
 
-router.get('/rps', dosenController.viewDosenRPS);
+router.get("/rps", dosenController.viewRPS);
+router.get("/dosen-rps", dosenController.viewDosenRPS);
+router.get("/dosen-cpmk", dosenController.viewDosenCPMK);
+router.get("/dosen-info-rps", dosenController.viewDosenInfoRPS);
+router.get("/dosen-komponen-nilai-tampil", dosenController.viewDosenKomponenNilaiTampil);
+router.get("/dosen-pertemuan", dosenController.viewDosenPertemuan);
+router.get("/dosen-referensi-a", dosenController.viewDosenReferensiA);
+router.get("/dosen-tambah-cpmk", dosenController.viewDosenTambahCPMK);
+router.get("/dosen-tambah-komponen", dosenController.viewDosenTambahKomponen);
+router.get("/dosen-tambah-pertemuan", dosenController.viewDosenTambahPertemuan);
+router.get("/dosen-tambah-referensi", dosenController.viewDosenTambahReferensi);
+router.get("/dosen-tambah-rps", dosenController.viewDosenTambahRPS);
 
 module.exports = router;
